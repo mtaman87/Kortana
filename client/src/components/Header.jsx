990e687488a0ee480352197309779e407ce41c4c
Marwan Taman <marwantaman87@gmail.com>
2023-03-12 15:21:25 -0400

    Fix image path

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Logo from "../../public/assets/k-logo-white.png";
 
 const Header = () => {
   return (
     <div className="h-20 w-[100vw] flex items-center text-[32px] text-white justify-center bg-slate-800 z-0">
-      <img className="m-2 w-[100px]" src={"../../public/assets/k-logo-white.png"} /> KORTANA
+      <img className="m-2 w-[100px]" src={Logo} /> KORTANA
     </div>
   );
 };
